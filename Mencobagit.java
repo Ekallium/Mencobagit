4b8fa47da1b91b4654f69a4a96b250f6d664cbba
Moch. Haikal Putra Muhajir <haikalputra1231@gmail.com>
2023-09-05 09:30:35 +0700

    begini benar kah?

---
+++ Mencobagit.java
@@ -0,0 +1,5 @@
+public class mencobagit {
+    public static void main(String[] args){
+        System.out.println("\t\t\t\t\t\tHalo Github !\nNama Saya Ekal\nDisini saya mau membuktikan tentang apakah bisa membuat folder terlebih dahulu baru meng upnya ke repository github?");
+    }
+}
